e56ac350ab6d7e6827d84c0dd44deb9d62959dad
hanabenasker <hanabenasker@insat.u-carthage.tn>
2022-12-09 07:46:07 +0100

    sonar ip address

--- script.groovy
+++ script.groovy
@@ -11,15 +11,15 @@ def runUnitTests() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'b@hnJ7uFP3csPz2', usernameVariable: 'hanabenasker')]) {
-        sh 'docker build -t hanabenasker/myrepo:""$BUILD_ID"" .'
+        sh 'docker build -t hanabenasker/myrepo:devim .'
         sh "echo 'b@hnJ7uFP3csPz2'| docker login -u 'hanabenasker' --password-stdin"
-        sh 'docker push hanabenasker/myrepo:""$BUILD_ID""'
+        sh 'docker push hanabenasker/myrepo:devim'
     }
 }
 
 def sonarTest() {
     echo "Running sonarQube checks..."
-    sh 'mvn clean verify sonar:sonar   -Dmaven.test.skip=true  -Dsonar.projectKey=project-devops   -Dsonar.host.url=http://172.18.0.4:9000   -Dsonar.login=sqp_7bb89c97447e1f73a6bbc15fd71805a3e42af942'
+    sh 'mvn clean verify sonar:sonar   -Dmaven.test.skip=true  -Dsonar.projectKey=project-devops   -Dsonar.host.url=http://172.18.0.6:9000   -Dsonar.login=sqp_7bb89c97447e1f73a6bbc15fd71805a3e42af942'
 }
 
 
